fef4fc7507efd98fe9c13a4c8148a3a5b66806e1
Alex McLain <alex@alexmclain.com>
2016-02-02 22:14:41 -0800

    Change mock output to default to zero.

--- mock_output.v
+++ mock_output.v
@@ -40,35 +40,36 @@ assign data = data_buf;
 always @(*)
 begin
   case (data_selector)
-    default: data_buf <= packet_0;
-    6'd01:   data_buf <= packet_1;
-    6'd02:   data_buf <= packet_2;
-    6'd03:   data_buf <= packet_3;
-    6'd04:   data_buf <= packet_4;
-    6'd05:   data_buf <= packet_5;
-    6'd06:   data_buf <= packet_6;
-    6'd07:   data_buf <= packet_7;
-    6'd08:   data_buf <= packet_8;
-    6'd09:   data_buf <= packet_9;
-    6'd10:   data_buf <= packet_10;
-    6'd11:   data_buf <= packet_11;
-    6'd12:   data_buf <= packet_12;
-    6'd13:   data_buf <= packet_13;
-    6'd14:   data_buf <= packet_14;
-    6'd15:   data_buf <= packet_15;
-    6'd16:   data_buf <= packet_16;
-    6'd17:   data_buf <= packet_17;
-    6'd18:   data_buf <= packet_18;
-    6'd19:   data_buf <= packet_19;
-    6'd20:   data_buf <= packet_20;
-    6'd21:   data_buf <= packet_21;
-    6'd22:   data_buf <= packet_22;
-    6'd23:   data_buf <= packet_23;
-    6'd24:   data_buf <= packet_24;
-    6'd25:   data_buf <= packet_25;
-    6'd26:   data_buf <= packet_26;
-    6'd27:   data_buf <= packet_27;
-    6'd28:   data_buf <= packet_28;
+    6'd00:   data_buf = packet_0;
+    6'd01:   data_buf = packet_1;
+    6'd02:   data_buf = packet_2;
+    6'd03:   data_buf = packet_3;
+    6'd04:   data_buf = packet_4;
+    6'd05:   data_buf = packet_5;
+    6'd06:   data_buf = packet_6;
+    6'd07:   data_buf = packet_7;
+    6'd08:   data_buf = packet_8;
+    6'd09:   data_buf = packet_9;
+    6'd10:   data_buf = packet_10;
+    6'd11:   data_buf = packet_11;
+    6'd12:   data_buf = packet_12;
+    6'd13:   data_buf = packet_13;
+    6'd14:   data_buf = packet_14;
+    6'd15:   data_buf = packet_15;
+    6'd16:   data_buf = packet_16;
+    6'd17:   data_buf = packet_17;
+    6'd18:   data_buf = packet_18;
+    6'd19:   data_buf = packet_19;
+    6'd20:   data_buf = packet_20;
+    6'd21:   data_buf = packet_21;
+    6'd22:   data_buf = packet_22;
+    6'd23:   data_buf = packet_23;
+    6'd24:   data_buf = packet_24;
+    6'd25:   data_buf = packet_25;
+    6'd26:   data_buf = packet_26;
+    6'd27:   data_buf = packet_27;
+    6'd28:   data_buf = packet_28;
+    default: data_buf = 8'h00;
   endcase
 end
 
